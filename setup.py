e61c1cd27069cece97ece446d70e6a743b05aeed
Vishnu <vishnu012@pm.me>
2022-01-25 13:17:48 +0530

    version updated to v0.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="opml2json",
-    version="0.0.3",
+    version="0.1.0",
     description="convert opml to json",
     long_description=README,
     long_description_content_type="text/markdown",
